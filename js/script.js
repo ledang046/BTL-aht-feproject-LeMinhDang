8dd907df313b689df24bb7bdeb3459c5bc1a7981
ledang046 <ledang046@gmail.com>
2021-09-01 09:46:58 +0700

    aht-project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.bar').click(function (e) { 
+        e.preventDefault();
+        $('.menu-navbar').addClass('active');
+        $('.cover').addClass('active');
+    });
+    $('.cover').click(function (e) { 
+        e.preventDefault();
+        $('.menu-navbar').removeClass('active');
+        $('.cover').removeClass('active');
+    });
+});
